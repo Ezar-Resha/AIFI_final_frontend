8a0e55092c491de87e6b9aca1a8966b306b72cb8
ezarresha <reshaezar@gmail.com>
2024-09-17 21:45:40 +0700

    commit final prototype

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,102 +1,78 @@
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet, Image, Platform } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, ScrollView, View, ActivityIndicator, RefreshControl } from "react-native";
+import Card from "../../components/cards.js";
+import * as FileSystem from "expo-file-system";
 
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const DIRECTORY_PATH = `${FileSystem.documentDirectory}WoodBlockApp/`;
+const JSON_FILE_PATH = `${DIRECTORY_PATH}data.json`;
 
 export default function TabTwoScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
-      </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
-        <Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText> to see how to load{' '}
-          <ThemedText style={{ fontFamily: 'SpaceMono' }}>
-            custom fonts such as this one.
-          </ThemedText>
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user's current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful <ThemedText type="defaultSemiBold">react-native-reanimated</ThemedText> library
-          to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
-    </ParallaxScrollView>
-  );
+    const [isLoading, setIsLoading] = useState(true);
+    const [data, setData] = useState([]);
+    const [isRefreshing, setIsRefreshing] = useState(false);
+
+    const loadData = async () => {
+        try {
+            const fileInfo = await FileSystem.getInfoAsync(JSON_FILE_PATH);
+
+            if (fileInfo.exists) {
+                const jsonData = await FileSystem.readAsStringAsync(JSON_FILE_PATH);
+                const parsedData = JSON.parse(jsonData);
+                setData(parsedData);
+            } else {
+                console.log("JSON file does not exist.");
+                setData([]);
+            }
+        } catch (error) {
+            console.error("Error reading JSON file:", error);
+        } finally {
+            setIsLoading(false);
+            setIsRefreshing(false);
+        }
+    };
+
+    useEffect(() => {
+        loadData();
+    }, []);
+    const onRefresh = () => {
+        setIsRefreshing(true);
+        loadData();
+    };
+    return (
+        <ScrollView
+            contentContainerStyle={styles.container}
+            refreshControl={<RefreshControl refreshing={isRefreshing} onRefresh={onRefresh} />}
+        >
+            <View style={styles.header}>
+                <Text style={styles.headerTitle}>History</Text>
+            </View>
+            {isLoading ? (
+                <ActivityIndicator size="large" color="#007bff" />
+            ) : data && data.length > 0 ? (
+                data.map((item, index) => (
+                    <Card key={index} data={item} /> // Render Card component for each item
+                ))
+            ) : (
+                <Text>No data available</Text>
+            )}
+        </ScrollView>
+    );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
-  },
+    container: {
+        flexGrow: 1,
+        paddingHorizontal: 8,
+        paddingVertical: 20,
+        backgroundColor: "#f8f8f8",
+    },
+    header: {
+        paddingHorizontal: 16,
+        marginBottom: 12,
+    },
+    headerTitle: {
+        fontSize: 32,
+        fontWeight: "700",
+        color: "#1d1d1d",
+    },
 });
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,285 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { View, TouchableOpacity, Text, StyleSheet, Alert, Image, ScrollView, TextInput } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons"; // Import icons from MaterialCommunityIcons
+import { uploadImage } from "../../api/api.js";
+import { useNavigation } from "expo-router";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const CALIBRATION_FILE_PATH = `${FileSystem.documentDirectory}calibration.json`; // Path to your calibration file
+const DATA_FILE_PATH = `${FileSystem.documentDirectory}WoodBlockApp/data.json`; // Path to data.json file
+const UPLOAD_FOLDER = `${FileSystem.documentDirectory}WoodBlockApp/upload/`; // Path to upload folder
+const PROCESSED_FOLDER = `${FileSystem.documentDirectory}WoodBlockApp/processed/`; // Path to processed folder
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+    const navigation = useNavigation();
+    const [image, setImage] = useState(null);
+    const [calibrationImage, setCalibrationImage] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            // Request camera and media library permissions on component mount
+            const { status: cameraStatus } = await ImagePicker.requestCameraPermissionsAsync();
+            const { status: mediaLibraryStatus } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+            if (cameraStatus !== "granted" || mediaLibraryStatus !== "granted") {
+                Alert.alert(
+                    "Permission Denied",
+                    "Sorry, we need camera and media library permissions to make this work!"
+                );
+            }
+        })();
+    }, []);
+
+    const handleTakePhoto = async () => {
+        try {
+            const result = await ImagePicker.launchCameraAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                allowsEditing: true,
+                quality: 1, // Adjust the quality of the image
+            });
+
+            if (!result.canceled) {
+                const selectedImage = result.assets[0]; // Store the image URI
+                setImage(selectedImage); // Save to state
+
+                // Read calibration data
+                const calibrationData = await readCalibrationData();
+
+                if (!calibrationData) {
+                    Alert.alert("Calibration data is missing or corrupted.");
+                    return;
+                }
+
+                // Calculate pixelToCmRatio from calibration data
+                const { pixelLength, actualSize } = calibrationData;
+                const pixelToCmRatio = actualSize / pixelLength;
+
+                // Call your uploadImage function with the selected image and calculated ratio
+                const response = await uploadImage(selectedImage, pixelToCmRatio);
+
+                // Navigate to the detail page with the result as a prop
+                navigation.navigate("detail", {
+                    originalImage: response.originalImage,
+                    processedImage: response.processedImage,
+                    block_details: response.block_details,
+                    total_blocks: response.total_blocks,
+                    g1_blocks: response.g1_blocks,
+                    non_g1_blocks: response.non_g1_blocks,
+                });
+            }
+        } catch (error) {
+            console.error("Error taking photo: ", error);
+        }
+    };
+
+    const handleSelectPhoto = async () => {
+        try {
+            const result = await ImagePicker.launchImageLibraryAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                allowsEditing: true,
+                quality: 1, // Adjust the quality of the image
+            });
+
+            if (!result.canceled) {
+                const selectedImage = result.assets[0]; // Store the image URI
+                setImage(selectedImage); // Save to state
+
+                // Read calibration data
+                const calibrationData = await readCalibrationData();
+
+                if (!calibrationData) {
+                    Alert.alert("Calibration data is missing or corrupted.");
+                    return;
+                }
+
+                // Calculate pixelToCmRatio from calibration data
+                const { pixelLength, actualSize } = calibrationData;
+                const pixelToCmRatio = actualSize / pixelLength;
+
+                // Call your uploadImage function with the selected image and calculated ratio
+                const response = await uploadImage(selectedImage, pixelToCmRatio);
+
+                // Navigate to the detail page with the result as a prop
+                navigation.navigate("detail", {
+                    originalImage: response.originalImage,
+                    processedImage: response.processedImage,
+                    block_details: response.block_details,
+                    total_blocks: response.total_blocks,
+                    g1_blocks: response.g1_blocks,
+                    non_g1_blocks: response.non_g1_blocks,
+                });
+
+                Alert.alert("Photo Uploaded Successfully!");
+            }
+        } catch (error) {
+            console.error("Error selecting photo: ", error);
+            Alert.alert("Error selecting photo!");
+        }
+    };
+
+    const readCalibrationData = async () => {
+        try {
+            const calibrationJson = await FileSystem.readAsStringAsync(CALIBRATION_FILE_PATH);
+            return JSON.parse(calibrationJson);
+        } catch (error) {
+            console.error("Error reading calibration data: ", error);
+            return null; // Return null if there is an error reading the file
+        }
+    };
+
+    const handleCalibrationPhoto = () => {
+        navigation.navigate("calibration");
+    };
+
+    const resetData = async () => {
+        try {
+            // Delete files in upload and processed folders
+            await deleteFolderFiles(UPLOAD_FOLDER);
+            await deleteFolderFiles(PROCESSED_FOLDER);
+
+            // Clear the data.json file
+            await FileSystem.writeAsStringAsync(DATA_FILE_PATH, JSON.stringify([], null, 2)); // Reset data.json to an empty array
+
+            Alert.alert("Data Reset", "All data has been cleared successfully.");
+        } catch (error) {
+            console.error("Error resetting data: ", error);
+            Alert.alert("Error", "An error occurred while resetting the data.");
+        }
+    };
+    const deleteFolderFiles = async (folderPath) => {
+        const folderInfo = await FileSystem.getInfoAsync(folderPath);
+        if (folderInfo.exists) {
+            const files = await FileSystem.readDirectoryAsync(folderPath);
+            for (const file of files) {
+                const filePath = `${folderPath}${file}`;
+                await FileSystem.deleteAsync(filePath);
+            }
+        }
+    };
+
+    return (
+        <ScrollView contentContainerStyle={styles.container}>
+            {/* Three Square Boxes for Actions */}
+            <View style={styles.boxContainer}>
+                {/* Calibration Box with Reset Button */}
+                <View style={styles.calibrationBoxContainer}>
+                    <TouchableOpacity style={styles.box} onPress={handleCalibrationPhoto}>
+                        <Icon name="magnify" size={40} color="#007bff" />
+                        <Text style={styles.boxText}>Calibration</Text>
+                    </TouchableOpacity>
+                    {/* Reset Data Button Positioned at the Bottom Left */}
+                    <TouchableOpacity style={styles.box} onPress={resetData}>
+                        <Text style={styles.boxText}>Reset Data</Text>
+                    </TouchableOpacity>
+                </View>
+
+                {/* Take Photo Box */}
+                <TouchableOpacity style={styles.box} onPress={handleTakePhoto}>
+                    <Icon name="camera" size={40} color="#28a745" />
+                    <Text style={styles.boxText}>Take Photo</Text>
+                </TouchableOpacity>
+
+                {/* Select Photo Box */}
+                <TouchableOpacity style={styles.box} onPress={handleSelectPhoto}>
+                    <Icon name="folder" size={40} color="#ff7f50" />
+                    <Text style={styles.boxText}>Select Photo</Text>
+                </TouchableOpacity>
+            </View>
+        </ScrollView>
+    );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+    container: {
+        flexGrow: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20,
+        backgroundColor: "#f8f8f8",
+    },
+    boxContainer: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+        width: "100%",
+        marginBottom: 20,
+    },
+    calibrationBoxContainer: {
+        flexDirection: "column", // Align reset button below the calibration box
+        alignItems: "center", // Center the calibration box horizontally
+        position: "relative", // Make it possible to use absolute positioning for the reset button
+    },
+    box: {
+        width: 100,
+        height: 100,
+        backgroundColor: "#f0f0f0",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 10,
+        elevation: 2,
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 5,
+        marginBottom: 16,
+    },
+    boxText: {
+        marginTop: 5,
+        fontSize: 14,
+        textAlign: "center",
+    },
+    calibrationContainer: {
+        marginBottom: 20,
+        alignItems: "center",
+    },
+    calibrationTitle: {
+        fontSize: 18,
+        fontWeight: "bold",
+        marginBottom: 10,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: "#ccc",
+        borderRadius: 5,
+        padding: 10,
+        width: 200,
+        marginVertical: 10,
+    },
+    imagePreviewContainer: {
+        marginBottom: 20,
+        alignItems: "center",
+    },
+    previewText: {
+        fontSize: 16,
+        marginBottom: 5,
+    },
+    imagePreview: {
+        width: 200,
+        height: 200,
+        borderRadius: 10,
+    },
+    resultContainer: {
+        marginTop: 20,
+        alignItems: "center",
+    },
+    resultImageContainer: {
+        marginBottom: 10,
+        alignItems: "center",
+    },
+    resultImage: {
+        width: 200,
+        height: 200,
+        borderRadius: 10,
+    },
+    resultText: {
+        fontSize: 16,
+        marginTop: 10,
+        textAlign: "center",
+    },
+    processButton: {
+        padding: 10,
+        marginTop: 10,
+        backgroundColor: "#28a745",
+        borderRadius: 5,
+    },
 });
